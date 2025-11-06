6b2578642befa371ada8f2590b3ce4626208d87a
Rob Stobbe <50251379+rstobbe@users.noreply.github.com>
2025-11-06 12:36:41 -0700

    update

---
+++ Image StitchIt/Underlying/Selectable Functions/Recon Functions/CpsReconYfpFbV2b.m
@@ -0,0 +1,185 @@
+%==================================================================
+% (V2b)
+%   - NavigatorFlip = input
+%==================================================================
+
+classdef CpsReconYfpFbV2b < handle
+
+properties (SetAccess = private)                   
+    Recon
+    CpsTrajMashObj
+    ReturnType
+    Shift
+end
+
+methods 
+   
+%==================================================================
+% Constructor
+%==================================================================  
+function obj = CpsReconYfpFbV2b()              
+end
+
+%==================================================================
+% CreateImage
+%==================================================================  
+function [IMG,err] = CreateImage(obj,DATA)     
+    [Image,err] = obj.Recon.CreateImage(DATA);
+    if err.flag
+        IMG = [];
+        return
+    end
+    %TrajMashPanel = obj.CpsTrajMashObj.ReturnInfoCompass();
+    Panel(1,:) = {'','','Output'};
+    Panel(2,:) = {'ReconObj',obj.Recon.Method,'Output'};
+    Panel(3,:) = {'ReconMatrix',obj.Recon.BaseMatrix,'Output'};
+    Panel(4,:) = {'Shift',obj.Shift,'Output'};
+    Panel(5,:) = {'','','Output'};
+    %Panel = cat(1,Panel,TrajMashPanel);
+    PanelOutput = cell2struct(Panel,{'label','value','type'},2);
+    
+    NameSuffix = 'ReconYfpV2a';
+    IMG = AddCompassInfo(Image,DATA{1}.DataObj,obj.Recon.AcqInfo{1},obj,PanelOutput,NameSuffix);         
+end
+
+%=================================================================
+% InitViaCompass
+%==================================================================  
+function InitViaCompass(obj,ReconIpt)    
+    obj.Recon = ReconYfpFbV2b();   
+    obj.Recon.SetBaseMatrix(str2double(ReconIpt.('ReconMatrix'))); 
+    if not(strcmp(ReconIpt.('ReturnFov'),'Full'))
+        obj.Recon.SetDoSaveSmallerFov(1);
+        obj.Recon.SetSaveSmallerFov(str2double(ReconIpt.('ReturnFov')));
+    end 
+    obj.Shift(1) = str2double(ReconIpt.('ShiftLr'));
+    obj.Shift(2) = str2double(ReconIpt.('ShiftTb'));
+    obj.Shift(3) = str2double(ReconIpt.('ShiftIo'));
+    obj.Recon.SetShift(obj.Shift);
+    if strcmp(ReconIpt.('LowRamCase'),'Yes')
+        obj.Recon.SetLowRamCase(1);
+    end 
+    if strcmp(ReconIpt.('LowGpuRamCase'),'Yes')
+        obj.Recon.SetLowGpuRamCase(1);
+    end 
+    if strcmp(ReconIpt.('DataTests'),'Yes')
+        obj.Recon.SetTestData(1);
+    end 
+    if strcmp(ReconIpt.('DisplayRxProfs'),'Yes')
+        obj.Recon.SetDisplayRxProfs(1);
+    end 
+    FlipStr = ReconIpt.('NavigatorFlip');
+    for n = 1:length(FlipStr)
+        Flip(n) = str2double(FlipStr(n));
+    end
+    obj.Recon.SetNavigatorFlip(Flip);
+
+    CallingLabel = ReconIpt.Struct.labelstr;
+    if not(isfield(ReconIpt,[CallingLabel,'_Data']))
+        if isfield(ReconIpt.('Recon_File').Struct,'selectedfile')
+            file = ReconIpt.('Recon_File').Struct.selectedfile;
+            if not(exist(file,'file'))
+                err.flag = 1;
+                err.msg = '(Re) Load Recon_File';
+                ErrDisp(err);
+                return
+            else
+                load(file);
+                ReconIpt.([CallingLabel,'_Data']).('Recon_File_Data') = saveData;
+            end
+        else
+            err.flag = 1;
+            err.msg = '(Re) Load Recon_File';
+            ErrDisp(err);
+            return
+        end
+    end
+    obj.Recon.SetAcqInfo(ReconIpt.([CallingLabel,'_Data']).('Recon_File_Data').WRT.STCH);
+    obj.Recon.SetAcqInfoRxp(ReconIpt.([CallingLabel,'_Data']).('Recon_File_Data').WRT.STCHRXP);
+
+    TrajMashfunc = ReconIpt.('TrajMashfunc').Func; 
+    TrajMashIpt = ReconIpt.('TrajMashfunc');
+    CallingFunction = ReconIpt.Struct.labelstr;
+    if isfield(ReconIpt,([CallingFunction,'_Data']))
+        if isfield(ReconIpt.([CallingFunction,'_Data']),('TrajMashfunc_Data'))
+            TrajMashIpt.TrajMashfunc_Data = ReconIpt.([CallingFunction,'_Data']).TrajMashfunc_Data;
+        end
+    end
+    func = str2func(TrajMashfunc);                   
+    obj.CpsTrajMashObj = func();
+    obj.CpsTrajMashObj.InitViaCompass(TrajMashIpt);
+    obj.Recon.SetTrajMashObj(obj.CpsTrajMashObj.TrajMashObj);    
+end
+
+%==================================================================
+% CompassInterface
+%==================================================================  
+function [Interface] = CompassInterface(obj,SCRPTPATHS)    
+    path = SCRPTPATHS.voyagerloc;
+    func = 'CpsTrajMashLungEndExp3e';
+    global COMPASSINFO
+    m = 1;
+    Interface{m,1}.entrytype = 'RunExtFunc';
+    Interface{m,1}.labelstr = 'Recon_File';
+    Interface{m,1}.entrystr = '';
+    Interface{m,1}.buttonname = 'Load';
+    Interface{m,1}.runfunc1 = 'LoadReconCur';
+    Interface{m,1}.(Interface{m,1}.runfunc1).curloc = SCRPTPATHS.outloc;
+    Interface{m,1}.runfunc2 = 'LoadReconDisp';
+    Interface{m,1}.(Interface{m,1}.runfunc2).defloc = COMPASSINFO.USERGBL.trajreconloc;
+    m = m+1;
+    Interface{m,1}.entrytype = 'Choose';
+    Interface{m,1}.labelstr = 'ReconMatrix';
+    Interface{m,1}.entrystr = '300';
+    mat = (10:10:500).';
+    Interface{m,1}.options = mat2cell(mat,length(mat));
+    m = m+1;
+    Interface{m,1}.entrytype = 'Input';
+    Interface{m,1}.labelstr = 'ReturnFov (mm)';
+    Interface{m,1}.entrystr = 'Full';
+    m = m+1;
+    Interface{m,1}.entrytype = 'Input';
+    Interface{m,1}.labelstr = 'ShiftIo (mm)';
+    Interface{m,1}.entrystr = '0';
+    m = m+1;
+    Interface{m,1}.entrytype = 'Input';
+    Interface{m,1}.labelstr = 'ShiftTb (mm)';
+    Interface{m,1}.entrystr = '0';
+    m = m+1;
+    Interface{m,1}.entrytype = 'Input';
+    Interface{m,1}.labelstr = 'ShiftLr (mm)';
+    Interface{m,1}.entrystr = '0';
+    m = m+1;
+    Interface{m,1}.entrytype = 'Choose';
+    Interface{m,1}.labelstr = 'LowRamCase';
+    Interface{m,1}.entrystr = 'No';
+    Interface{m,1}.options = {'Yes','No'};
+    m = m+1;
+    Interface{m,1}.entrytype = 'Choose';
+    Interface{m,1}.labelstr = 'LowGpuRamCase';
+    Interface{m,1}.entrystr = 'No';
+    Interface{m,1}.options = {'Yes','No'};
+    m = m+1;
+    Interface{m,1}.entrytype = 'Input';
+    Interface{m,1}.labelstr = 'NavigatorFlip';
+    Interface{m,1}.entrystr = '0';
+    m = m+1;
+    Interface{m,1}.entrytype = 'Choose';
+    Interface{m,1}.labelstr = 'DataTests';
+    Interface{m,1}.entrystr = 'No';
+    Interface{m,1}.options = {'Yes','No'};
+    m = m+1;
+    Interface{m,1}.entrytype = 'Choose';
+    Interface{m,1}.labelstr = 'DisplayRxProfs';
+    Interface{m,1}.entrystr = 'No';
+    Interface{m,1}.options = {'Yes','No'};
+    m = m+1;
+    Interface{m,1}.entrytype = 'ScrptFunc';
+    Interface{m,1}.labelstr = 'TrajMashfunc';
+    Interface{m,1}.entrystr = func;
+    Interface{m,1}.searchpath = path;
+    Interface{m,1}.path = [path,func];
+end 
+
+end
+end
---
+++ Image StitchIt/Underlying/Selectable Functions/TrajMash Functions/CpsTrajMashLungEndExp3e.m
@@ -0,0 +1,72 @@
+%==================================================================
+% (V2e)
+%   - Flip moved up to recon
+%==================================================================
+
+classdef CpsTrajMashLungEndExp3e < handle
+
+properties (SetAccess = private)                   
+    TrajMashObj
+end
+
+methods 
+   
+%==================================================================
+% Constructor
+%==================================================================  
+function obj = CpsTrajMashLungEndExp3e()              
+end
+
+%=================================================================
+% InitViaCompass
+%==================================================================  
+function InitViaCompass(obj,Ipt)    
+    obj.TrajMashObj = TrajMashLungEndExp3e();   
+    obj.TrajMashObj.SetStartSkip(str2double(Ipt.('StartSkip')));
+    obj.TrajMashObj.SetDispFigs(str2double(Ipt.('DispFigs')));
+    obj.TrajMashObj.SetPeakFindSensitivity(str2double(Ipt.('PeakFindSensitivity')));
+    obj.TrajMashObj.SetAtExpirationFrac(str2double(Ipt.('AtExpirationFrac')));
+end
+
+%=================================================================
+% ReturnInfoCompass
+%==================================================================  
+function Panel = ReturnInfoCompass(obj) 
+    Panel(1,:) = {'TrajMashObj',obj.TrajMashObj.Method,'Output'};
+    Panel(2,:) = {'StartSkip',obj.TrajMashObj.StartSkip,'Output'};
+    Panel(3,:) = {'FilterTime',obj.TrajMashObj.FilterTime,'Output'};
+    Panel(4,:) = {'AtExpirationFrac',obj.TrajMashObj.AtExpirationFrac,'Output'};
+    Panel(5,:) = {'AtExpirationPerFrac',obj.TrajMashObj.AtExpirationPeriFrac,'Output'};
+    Panel(6,:) = {'PeakFindSensitivity',obj.TrajMashObj.PeakFindSensitivity,'Output'};   
+    Panel(7,:) = {'MeanAvesUsedPerTraj',obj.TrajMashObj.MeanTrajsUsed,'Output'};
+    Panel(8,:) = {'PeriValsFraction',obj.TrajMashObj.PeriValsFraction,'Output'};
+    Panel(9,:) = {'HoleFraction',obj.TrajMashObj.HoleFraction,'Output'};
+    Panel(10,:) = {'CoilUsed',obj.TrajMashObj.UseCoil,'Output'};
+end
+
+%==================================================================
+% CompassInterface
+%==================================================================  
+function [Interface] = CompassInterface(obj,SCRPTPATHS)    
+    m = 1;
+    Interface{m,1}.entrytype = 'Input';
+    Interface{m,1}.labelstr = 'StartSkip';
+    Interface{m,1}.entrystr = '1';
+    m = m+1;
+    Interface{m,1}.entrytype = 'Choose';
+    Interface{m,1}.labelstr = 'DispFigs';
+    Interface{m,1}.entrystr = '1';
+    Interface{m,1}.options = {'0','1','2'};
+    m = m+1;
+    Interface{m,1}.entrytype = 'Choose';
+    Interface{m,1}.labelstr = 'PeakFindSensitivity';
+    Interface{m,1}.entrystr = '2';
+    Interface{m,1}.options = {'1','2','3','4','5','6','7','8'};
+    m = m+1;
+    Interface{m,1}.entrytype = 'Input';
+    Interface{m,1}.labelstr = 'AtExpirationFrac';
+    Interface{m,1}.entrystr = '0.25';
+end 
+
+end
+end
